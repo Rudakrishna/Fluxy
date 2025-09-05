1b067e6b55d0c3556ee3ef5607d5bd2faa56ea9c
Rudakrishna <Rudakrishna@users.noreply.github.com>
2025-09-05 18:20:42 +0530

    Update install_comfyui.sh

--- build/install_comfyui.sh
+++ build/install_comfyui.sh
@@ -11,8 +11,7 @@ python3 -m venv --system-site-packages venv
 source venv/bin/activate
 
 # Install torch, xformers and sageattention
-pip3 install --no-cache-dir torch=="${TORCH_VERSION}" torchvision torchaudio --index-url ${INDEX_URL}
-pip3 install --no-cache-dir xformers=="${XFORMERS_VERSION}" --index-url ${INDEX_URL}
+pip3 install --no-cache-dir torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu129
 
 # Install requirements
 pip3 install -r requirements.txt
